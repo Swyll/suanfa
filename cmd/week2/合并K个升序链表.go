6e03dcf9e9c13718bb6f4854286f891f5f366a40
liulei <89353951@qq.com>
2022-04-25 20:31:15 +0800

    关于时间复杂度的问题

--- cmd/week2/合并K个升序链表.go
+++ cmd/week2/合并K个升序链表.go
@@ -46,6 +46,7 @@ type ListNode struct {
 	Next *ListNode
 }
 
+//分治
 func mergeKLists(lists []*ListNode) *ListNode {
 	//给过来的数组可能有空数组，要考虑到该边界条件
 	if len(lists) < 1 {
@@ -61,6 +62,24 @@ func mergeKLists(lists []*ListNode) *ListNode {
 	r2 := mergeKLists(lists[length/2 : length])
 
 	return merge(r1, r2)
+}
+
+//question:
+//                                                             ++++++++++++++助教大大帮忙解答一下+++++++++++++++++
+
+//为什么迭代的时间复杂度会更高呢?按照该题，分治的操作次数为n-1(设x=logn,操作次数为y，那么y=2^x-1+x^x-2+...+2^1+1=n-1),而迭代的操作次数也是n，当n足够大时，两个操作次数相同
+//在每个操作节点内，分治也没有减少重复的操作，合并的复杂度也基本一致，但是为什么执行时间会相差这么多呢?
+func mergeKLists2(lists []*ListNode) *ListNode {
+	if len(lists) == 1 {
+		return lists[0]
+	}
+	var head *ListNode
+
+	for i := 0; i < len(lists); i++ {
+		head = merge(head, lists[i])
+	}
+
+	return head
 }
 
 func merge(list1, list2 *ListNode) *ListNode {
